e2388ecfe5a831c104b2870f7686bde9ec2b9180
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-01-21 17:34:43 +0100

    First commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,54 @@
+﻿partial class MainForm
+{
+    private System.ComponentModel.IContainer components = null;
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FaceRecognizer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+    }
+
+    private System.Windows.Forms.PictureBox pictureBox1;
+}
---
+++ MainForm.cs
@@ -0,0 +1,100 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Diagnostics;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using FaceRecognitionSharp;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+public partial class MainForm : Form
+{
+    [DllImport("psapi.dll")]
+    static extern int EmptyWorkingSet(IntPtr hwProc);
+
+    [DllImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool SetProcessWorkingSetSize(IntPtr process, UIntPtr minimumWorkingSetSize, UIntPtr maximumWorkingSetSize);
+
+    private VideoCapture _capture;
+    private Mat _frame;
+
+    public MainForm()
+    {
+        InitializeComponent();
+        FaceRecognition.Initialize();
+        CheckForIllegalCrossThreadCalls = false;
+        Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.RealTime;
+
+        _frame = new Mat();
+        _capture = new VideoCapture(0);
+        _capture.Open(0);
+
+        Thread captureCameraThread = new Thread(CaptureCamera);
+        captureCameraThread.Priority = ThreadPriority.Highest;
+        captureCameraThread.Start();
+
+        Thread clearRamThread = new Thread(ClearRam);
+        clearRamThread.Priority = ThreadPriority.Highest;
+        clearRamThread.Start();
+    }
+
+    public void ClearRam()
+    {
+        while (true)
+        {
+            Thread.Sleep(100);
+            EmptyWorkingSet(Process.GetCurrentProcess().Handle);
+            GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+            GC.Collect(GC.MaxGeneration);
+            GC.WaitForPendingFinalizers();
+            SetProcessWorkingSetSize(Process.GetCurrentProcess().Handle, (UIntPtr)0xFFFFFFFF, (UIntPtr)0xFFFFFFFF);
+        }
+    }
+
+    public void CaptureCamera()
+    {
+        if (_capture.IsOpened())
+        {
+            while (true)
+            {
+                _capture.Read(_frame);
+                pictureBox1.Image = FaceRecognition.DrawRectsAndLandmarksToFaces(ResizeImage(BitmapConverter.ToBitmap(_frame), pictureBox1.Width, pictureBox1.Height), new DlibDotNet.RgbPixel(255, 255, 255), 1, new DlibDotNet.RgbPixel(255, 255, 0), 4);
+            }
+        }
+    }
+
+    public static Bitmap ResizeImage(Image image, int width, int height)
+    {
+        var destRect = new Rectangle(0, 0, width, height);
+        var destImage = new Bitmap(width, height);
+
+        destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+        using (var graphics = Graphics.FromImage(destImage))
+        {
+            graphics.CompositingMode = CompositingMode.SourceCopy;
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            using (var wrapMode = new ImageAttributes())
+            {
+                wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+            }
+        }
+
+        return destImage;
+    }
+
+    private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        Process.GetCurrentProcess().Kill();
+    }
+}
